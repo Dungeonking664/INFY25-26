902035ebf9b0dada650bb0fe10280cfe64908904
Dungeonking644 <CER240098@spengergasse.at>
2025-10-12 08:52:57 +0200

    select Hü

---
+++ 2025-10-09_select_customers/customers.sql
@@ -0,0 +1,9 @@
+select count() from customers where city = 'Berlin';
+
+select CustomerName , City , Country from customers;
+
+select * from customers where City = 'London';
+
+select * from products where Price >20;
+
+select * from products where Price != 18;
