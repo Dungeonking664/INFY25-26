939c662f6589438ea79b55e232ad890a0813f134
Dungeonking644 <CER240098@spengergasse.at>
2025-11-08 17:55:09 +0100

    opentrivia/hü.sql

---
+++ 2025-11-06_sqlite_mit_opentrivia/hü.sql
@@ -0,0 +1,9 @@
+SELECT SUM(1) from answer;
+
+select avg(opentdb_id) from category;
+
+SELECT question FROM Question WHERE question LIKE 'Who%';
+
+SELECT question FROM Question WHERE question LIKE '%capital%';
+
+SELECT name FROM Category WHERE name LIKE 'S%';
